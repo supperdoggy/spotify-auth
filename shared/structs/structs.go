0bcc7169bacd02890813cefcb4d67e16d737bc0c
supperdoggy <supperspggy229@gmail.com>
2021-12-05 18:55:23 +0200

    structs update

--- shared/structs/structs.go
+++ shared/structs/structs.go
@@ -22,6 +22,8 @@ type CheckTokenResp struct {
 
 type RegisterReq struct {
 	Email string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
